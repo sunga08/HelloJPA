8c086d78dcb92d440799571a12d8a4720273a8e6
sunga <eert2260@naver.com>
2023-02-18 11:26:16 +0900

    final commit

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -18,33 +21,13 @@ public class JpaMain {
 
         try {
 
-            Member2 member = new Member2();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homCity", "street", "10000"));
-            member.getFavoriteFoods().add("떡볶이");
-            member.getFavoriteFoods().add("피자");
-            member.getFavoriteFoods().add("곱창");
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member2> query = cb.createQuery(Member2.class);
 
-            member.getAddressHistory().add(new AddressEntity("old1", "street2", "10001"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street2", "10002"));
+            Root<Member2> m = query.from(Member2.class);
 
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("==========START===========");
-            Member2 findMember = em.find(Member2.class, member.getId()); //지연로딩임
-
-            //Address a = findMember.getHomeAddress();
-            //findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            //치킨 -> 한식 변경
-            //findMember.getFavoriteFoods().remove("떡볶이");
-            //findMember.getFavoriteFoods().add("한식");
-
-            //findMember.getAddressHistory().remove(new Address("old1", "street2", "10001")); //equals를 정확히 넣어줘야 제대로 동작
-            //findMember.getAddressHistory().add(new Address("newCity1", "street", "10001"));
+            CriteriaQuery<Member2> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+            List<Member2> resultList = em.createQuery(cq).getResultList();
 
             tx.commit();
 
